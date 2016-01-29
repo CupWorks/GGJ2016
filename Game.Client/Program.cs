14b9ee3f95279ad080bd6d1999fadd26502e9fb5
Christoph Becher <christoph.becher@live.de>
2016-01-29 19:00:43 +0100

    WIP

---
+++ Game.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Game.Client
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.ReadKey(false);
+        }
+    }
+}
