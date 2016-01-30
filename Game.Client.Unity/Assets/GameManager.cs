a9405c25736c155447692d68a7ba204a696d0e01
Agata Raap <a.raap@verlagsgruppe-oetinger.de>
2016-01-30 19:29:14 +0100

    GameManager: GetInput

---
+++ Game.Client.Unity/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+
+	public void GetInput (string Input)
+	{
+		Debug.Log (Input);
+	}
+}
