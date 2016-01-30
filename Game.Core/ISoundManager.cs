dd19e5bc5ee2839274926b6b4e92555605ef00aa
M. Petra Baranski <info@progsource.de>
2016-01-30 13:01:33 +0100

    SoundManager: added stopSound and stopLoop

--- Game.Core/ISoundManager.cs
+++ Game.Core/ISoundManager.cs
@@ -3,6 +3,8 @@
     public interface ISoundManager
     {
         void PlaySound(string soundFile);
+        void StopSound();
         void PlayLoop(string soundFile);
+        void StopLoop();
     }
 }
