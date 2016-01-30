acbcf1a5ebf3da9c55d4adb52ba662eb3bc79c0d
M. Petra Baranski <info@progsource.de>
2016-01-30 13:46:56 +0100

    added Sound by actions

--- Game.Core/Game.cs
+++ Game.Core/Game.cs
@@ -87,6 +87,12 @@ namespace Game.Core
         private void PerformAction(Action action)
         {
             Output.WriteLine(CleanText(action.Text), OutputType.Action);
+
+            if (!string.IsNullOrEmpty(action.Sound))
+            {
+                SoundManager.PlaySound(action.Sound);
+            }
+
             if (string.IsNullOrEmpty(action.NextStep))
             {
                 Input.Request();
